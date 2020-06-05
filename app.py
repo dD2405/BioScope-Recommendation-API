4edce52b459b8aa80d5ab1de3ae8fa908040e049
Deepak Das <deepakdas1998@gmail.com>
2020-06-05 12:59:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,17 +51,19 @@ def recommend(title):
 
                 return recommendation_data.to_dict('records')
 
-'''from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
         
-@app.route('/movie', methods=['POST'])
+@app.route('/movie', methods=['GET'])
 def recommend_movies():
         res = recommend(request.args.get('title'))
         return jsonify(res)
 
 if __name__=='__main__':
-        app.run(port = 5000, debug = True)'''
+        app.run(port = 5000, debug = True)
 
-print(recommend('Batman v Superman: Dawn of Justice'))
+#print(recommend('Batman v Superman: Dawn of Justice'))
 
