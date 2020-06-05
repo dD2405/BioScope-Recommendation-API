10f11cd3a5ae61e0fe979a8628be5063e9c4f2af
Deepak Das <deepakdas1998@gmail.com>
2020-06-05 19:04:51 +0530

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -67,4 +67,4 @@ def results(movie_name):
         recommendations = recommend_movies(movie_name)
         return recommendations.to_dict('records')
 
-print(results('stree'))
+#print(results('stree'))
