56d59dd5e5acb954124afed0c5029cea022f0e2f
Deepak Das <deepakdas1998@gmail.com>
2020-06-07 17:41:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,request,jsonify
+from flask_cors import CORS
 import recommendation
 
 app = Flask(__name__)
+CORS(app) 
         
 @app.route('/movie', methods=['GET'])
 def recommend_movies():
